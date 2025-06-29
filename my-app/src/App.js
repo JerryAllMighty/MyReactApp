e193e131763e6b7ecf4a9f8152a069f50ceefba7
JerryAllMighty <dbsalszz@naver.com>
2025-06-29 20:38:46 +0900

    Route 활용 컴포넌트 추가

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,25 @@
 // import logo from './logo.svg';
 // import './App.css';
 import Home from "./pages/Home";
+import About from "./pages/About";
+import React from "react";
+import {Routes, Route, Link} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <Home>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />}>
+          <Route path="/about" element={<About />}>
+          </Route>
+        </Routes>
+      </Home>
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
